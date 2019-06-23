0232e9c815d31b413bd3ac3856e92f2528da4794
EM @YEHEY <em@king.net>
2019-06-23 10:07:28 -0400

    Updated "Discover More People ex. @Yehey"
    Updated "Discover More People ex. @Yehey"

--- src/client/components/Sidebar/InterestingPeople.js
+++ src/client/components/Sidebar/InterestingPeople.js
@@ -19,7 +19,7 @@ const InterestingPeople = ({ users, onRefresh }) => (
       {users && users.map(user => <User key={user.name} user={user} />)}
       <h4 className="InterestingPeople__more">
         <Link to={'/discover'}>
-          <FormattedMessage id="discover_more_people" defaultMessage="Discover More People" />
+          <FormattedMessage id="discover_more_people" defaultMessage="Discover More People ex. @Yehey" />
         </Link>
       </h4>
     </div>
