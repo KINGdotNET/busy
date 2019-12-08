81c42097bec6777ddd00a63c5d0760f9c62121e4
EM @YEHEY <em@king.net>
2019-12-07 19:22:19 -0500

    Update steemAPI.js

--- src/server/steemAPI.js
+++ src/server/steemAPI.js
@@ -1,6 +1,6 @@
 const createClient = require('lightrpc').createClient;
 
-const client = createClient(process.env.STEEMJS_URL || 'https://anyx.io.com');
+const client = createClient(process.env.STEEMJS_URL || 'https://api.steemit.com');
 client.sendAsync = (message, params) =>
   new Promise((resolve, reject) => {
     client.send(message, params, (err, result) => {
